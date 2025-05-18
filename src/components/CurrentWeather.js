a331b18aef9e8814881d6cc09a1d58915ea6e34a
김채현 <23011884@sju.ac.kr>
2025-05-18 18:23:51 +0900

    weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -6,12 +6,36 @@ import {
 } from "./styles/StyledComponents";
 import { getWeatherDescription } from "../utils/weather";
 
+const getCurrentIndex = (timeArray) =>{
+  const now = new Date();
+  return timeArray.findIndex((t)=>new Date(t)>now)-1;
+};
+
 const CurrentWeather = ({ weatherData, isLoading }) => {
   if (isLoading) {
-    return <div>채워주세요</div>;
+    return <div>날씨 정보를 불러오는 중...</div>;
   }
+  /* 스켈레톤 컴포넌트 적용해보기 */
+  let temperature = null; // let 값 변경 가능, 재선언 불가
+  let code = null;
 
-  return <div>채워주세요</div>;
+    if (weatherData && weatherData.hourly) {
+      const timeArray=weatherData.hourly.time;
+      const idx=getCurrentIndex(timeArray);
+      if(idx>=0){
+    temperature = weatherData.hourly.temperature_2m[idx];
+    code = weatherData.hourly.weather_code[idx];
+  }}
+  // 실제 날씨 정보 꺼냄
+
+  return (
+    <div>
+      <CurrentWeatherWrapper>
+        <Temperature>{temperature}°C</Temperature>
+        <WeatherCode>{getWeatherDescription(code)}</WeatherCode>
+      </CurrentWeatherWrapper>
+    </div>
+  );
 };
 
 export default CurrentWeather;
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -20,13 +20,50 @@ export const getWeatherDescription = (code) => {
 };
 
 export const formatHourlyData = (weatherData) => {
+  // API 원시 데이터를 시간별 예보 형식으로 처리
   if (!weatherData) return [];
-  // 밑에 코드 채워주세요
-  return [];
+
+  /* const times = weatherData.hourly.time;
+  const temperatures = weatherData.hourly.temperature_2m;
+  const codes = weatherData.hourly.weather_code;
+  구조분해할당을 활용하면 가독성이 좋아짐 */
+
+  const { time, temperature_2m, weather_code } = weatherData.hourly;
+  // weatherData.hourly 객체 안에서 time, temperature_2m, weather_code를 꺼내서 같은 이름의 변수로 한 번에 선언함
+  const result = [];
+  for (let i = 0; i < 12; i++) {
+    /* const str = parseInt(times[i].slice(11, 13), 10) + "시";
+    parseInt: 문자열을 정수로 바꿔주는 함수 */
+    const str = new Date(time[i]).getHours() + "시"
+    result.push({
+      time: str,
+      temperature: temperature_2m[i],
+      code: weather_code[i],
+    });
+  }
+  return result;
 };
 
 export const formatDailyData = (weatherData) => {
   if (!weatherData) return [];
-  // 밑에 코드 채워주세요
-  return [];
+
+  /* const dates = weatherData.daily.time;
+  const temperatures = weatherData.daily.temperature_2m_max;
+  const codes = weatherData.daily.weather_code; */
+
+  const { time: dates, temperature_2m_max: temperatures, weather_code: codes } = weatherData.daily;
+  const result = [];
+  for (let i = 0; i < 7; i++) {
+    const dateObj = new Date(dates[i]);
+    const month = dateObj.getMonth() + 1;
+    const day = dateObj.getDate();
+    const weekday = dateObj.toLocaleDateString("ko-KR", { weekday: "short" });
+    const formatdate = `${month}월 ${day}일 (${weekday})`;
+    result.push({
+      date: formatdate,
+      code: codes[i],
+      temperature: temperatures[i],
+    });
+  }
+  return result;
 };
